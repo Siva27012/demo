7d41b45e47619d1e3353e94e57d8eada199fab46
siva27012 <sivabala2701@gmail.com>
2024-06-17 11:42:46 +0530

    day1 code

---
+++ day1.py
@@ -0,0 +1,39 @@
+print("welcome")
+
+# comment 
+1
+'''comment '''
+
+# variable
+num1=10.34
+num1=12.34
+print(type(num1))
+print("num1",num1)
+
+com=1+5j
+print(type(com))
+print(com)
+# string 
+# sangeetha 
+# c
+
+name="welcome"
+print(type(name))
+print(name)
+
+
+#list
+
+list1=[1,2,3,4,5,6]
+
+list1=[1,2,3,4,5,6,7,8,9,10]
+#indexing
+print(list1)
+print(list1[5])
+print(list1[-6])
+#slicing
+print(list1[0:6])
+print(list1[:6])
+print(list1[2:5])
+print(list1[2:])
+list1=[1,2,3,4,5,6]
